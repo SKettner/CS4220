1f4c90d2ee4d247ec1fdecf30f008251f1f330da
SKettner <skettner@uccs.edu>
2024-02-03 10:21:24 -0700

    Started with new SSL Client and Server

--- tcp_client.c
+++ tcp_client.c
@@ -1,264 +1,98 @@
-/*tcp_client.c*/
-
-#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/select.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/sendfile.h>
-#include <time.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
-//definitions used throughou the file
-#define ISVALIDSOCKET(s) ((s) >= 0)
-#define CLOSESOCKET(s) close(s)
-#define SOCKET int
-#define GETSOCKETERRNO() (errno)
-
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-
-
-int main()
+int create_socket(const char *ip_address, int port)
 {
-    //Vars for later use
-    int clientSocket;
-    char buffer[1024];
-    struct sockaddr_in serverAddr;
-    socklen_t addr_size;
-    
-    char address_buffer[100];
-    char service_buffer[100];
-    struct timeval tv;
-    tv.tv_sec = 15; 
-    
-    
+    int s;
+    struct sockaddr_in addr;
 
-    //this creates a socket that use IPV4(PF_INET), TCP(SOCK_STREAM),
-    //and the best protocol (0)
-    printf("Creating socket...\n");
-    clientSocket = socket(PF_INET, SOCK_STREAM, 0); 
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip_address, &addr.sin_addr);
 
-    setsockopt(clientSocket, SOL_SOCKET, SO_SNDTIMEO, (struct timeval *)&tv, sizeof(struct timeval));
-
-    //if this socket creation is not successful
-    if (!ISVALIDSOCKET(clientSocket)) 
-    {
-        printf("Failed to create Socket.\n");
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+        perror("Unable to create socket");
+        exit(EXIT_FAILURE);
     }
-    else
-    {
-        printf("Created Socket.\n");
-      
 
-        //The following code was copied exactly from https://www.programminglogic.com/example-of-client-server-program-in-c-using-sockets-and-tcp/
-        //However this seems to be setting up where and how the 
-        //the client will be conecting with the server
-        
-        /*---- Configure settings of the server address struct ----*/
-        /* Address family = Internet */
-        serverAddr.sin_family = AF_INET;
-        /* Set port number, using htons function to use proper byte order */
-        serverAddr.sin_port = htons(9004);
-        /* Set IP address to localhost */
-        serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-        /* Set all bits of the padding field to 0 */
-        memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero); 
-
-        //This is where we attempt to connect to the server 
-        printf("Connecting...\n");
-        addr_size = sizeof serverAddr;
-        int testing = connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-        
-        //If the connection succeeds 
-        if(testing == 0)
-        {
-            printf("Connected.\n");
-
-
-            //declare the file we are opening
-            FILE* file = fopen("README.txt", "r");
-
-            //takes us to end of file
-            fseek(file, 0L, SEEK_END);
-
-            //finds length of file
-            long int fileSize = ftell(file);
-
-            //takes us to beginning of file
-            fseek(file, 0, SEEK_SET);
-
-            printf("Sending: \n");
-
-            //this var will save the data we plan on sending 
-            char data[fileSize];
-
-            //read in all data from file
-            fread(data, 1, fileSize, file);
-
-            //two pointers we will use to keep track of postions of data we are sending in the fi;e
-            char* currentData = data;
-            char* lastAckData = data;
-
-            //this figures out how many packages we have to send
-            int N = fileSize/1024;
-
-            //close the file
-            fclose(file);
-
-
-            //this keeps track of how many packages we have sent since reciving a reply
-            int sendBase = 0;
-            int nextSeqNum = 0;
-
-
-            //keeps track of time since packets sent
-            clock_t startTime = clock();
-
-           
-            //as long as we have stuff to send and have not recived reply that it was recived
-            while(1)
-            {
-                
-                //for every packet that has not yet been sent and recived reply for
-                while(nextSeqNum<(sendBase+N))
-                {
-                    //this will be the part of the packet the dictates it order 
-                    char currentSeqNum[10];
-                    sprintf(currentSeqNum, "%d", nextSeqNum);
-                    strcat(currentSeqNum,"---------");
-
-                    //this will store all data being sent in the packet
-                    char dataToSend[1024];
-                    strcat(dataToSend, currentSeqNum);
-
-                    //this will store the data from the file to send in the packet
-                    char temp[1013];
-                    strncpy(temp, currentData, 1013);
-
-                    //this will combine all data into packet
-                    strcat(dataToSend, temp);
-                    dataToSend[1024] = '\0';
-
-
-                    //send all data in current packet it is sent dataToSend over clientSocket
-                    //size is 1024 bytes
-                    size_t dataSizeSend = send(clientSocket, dataToSend, 1024, 0);
-
-                    //this tells us the next data and packet to send 
-                    currentData += 1013;
-                    nextSeqNum++;
-
-                    //this clears out all arrays
-                    bzero(currentSeqNum, 10);
-                    bzero(temp, 1013);
-                    bzero(dataToSend, 1024);
-                }
-
-                //waits for reply of server for 15 secs
-                int anyMoreDataRecived = recv(clientSocket, buffer, 1024, 0);
-
-                //if we get a reply
-                if(anyMoreDataRecived>0)
-                {
-                    //this finds the current packet that is not know to have been recived
-                    sendBase = 1 + atoi(buffer);
-                    lastAckData += (int)(sendBase*1013);
-                    
-                    //if we have no more packets to send
-                    if(sendBase >= N)
-                    {
-                        //end the loop as this means we were succsessful
-                        break;
-                    }
-                    //we that the next packet needed is the next one we are going to send
-                    if(!(sendBase == nextSeqNum))
-                    {
-                        //reset timer since this is what should happen
-                        startTime = clock();
-
-                    }
-                }
-            
-                //if 15 seconds have passed without a reply, or the reply was wrong
-                //and 15 seconds have passed 
-                if((clock() - startTime) > 15000)
-                {
-                    //reset the timer
-                    startTime = clock();
-
-                    //this will store data and return it incase these packets also fail
-                    char * temp = lastAckData;
-                    int temp2 = sendBase;
-
-                    //for every packet we need to send
-                    while(sendBase<=nextSeqNum)
-                    {
-
-                         //this will be the part of the packet the dictates it order 
-                        char currentSeqNum[10];
-                        sprintf(currentSeqNum, "%d", sendBase);
-                        strcat(currentSeqNum,"---------");
-
-                        //this will store all data being sent in the packet
-                        char dataToSend[1024];
-                        strcat(dataToSend, currentSeqNum);
-
-                        //this will store the data from the file to send in the packet
-                        char temp[1013];
-                        strncpy(temp, lastAckData, 1013);
-
-
-                        //this will combine all data into packet
-                        strcat(dataToSend, temp);
-                        dataToSend[1024] = '\0';
-
-
-                        //send all data in current packet it is sent dataToSend over clientSocket
-                        //size is 1024 bytes
-                        size_t dataSizeSend = send(clientSocket, dataToSend, 1024, 0);
-
-                        //this tells us the next data and packet to send 
-                        currentData += 1013;
-                        nextSeqNum++;
-
-                          //this clears out all arrays
-                        bzero(currentSeqNum, 10);
-                        bzero(temp, 1013);
-                        bzero(dataToSend, 1024);
-                    }
-
-                    //reset all data back to last know recived 
-                    lastAckData = temp;
-                    sendBase = temp2;
-
-                }
-
-                //clear buffer
-                bzero(buffer, 10);
-
-            }
-
-            printf("Closing socket...\n");
-            CLOSESOCKET(clientSocket);
-
-            printf("Finished.\n");
-            return 0;
-        }
-        else
-        {
-            printf("Connection Failed.\n");
-        }
+    if (connect(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("Unable to connect");
+        exit(EXIT_FAILURE);
     }
-}    
-	    
-	    
+
+    return s;
+}
+
+SSL_CTX *create_context()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+
+    method = TLS_client_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx)
+{
+    // No additional configuration is needed for the client context
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_ip> <server_port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *server_ip = argv[1];
+    int server_port = atoi(argv[2]);
+
+    int sock;
+    SSL_CTX *ctx;
+    SSL *ssl;
+
+    ctx = create_context();
+    configure_context(ctx);
+
+    sock = create_socket(server_ip, server_port);
+
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sock);
+
+    if (SSL_connect(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+    } else {
+        char buffer[1024];
+        SSL_read(ssl, buffer, sizeof(buffer));
+        printf("Received: %s\n", buffer);
+    }
+
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    close(sock);
+    SSL_CTX_free(ctx);
+
+    return 0;
+}
 	    
 	    
 	    
--- tcp_server.c
+++ tcp_server.c
@@ -1,175 +1,112 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/select.h>
-
-
-//definitions used throughou the file
-#define ISVALIDSOCKET(s) ((s) >= 0)
-#define CLOSESOCKET(s) close(s)
-#define SOCKET int
-#define GETSOCKETERRNO() (errno)
-
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
-int main() 
+int create_socket(int port)
 {
-	
-    //Vars for later use
-    int welcomeSocket, newSocket;
-    char buffer[1024];
-    struct sockaddr_in serverAddr;
-    struct sockaddr_storage serverStorage;
-    socklen_t addr_size;
+    int s;
+    struct sockaddr_in addr;
 
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    //this creates a socket that use IPV4(PF_INET), TCP(SOCK_STREAM),
-    //and the best protocol (0)
-    printf("Creating socket...\n");
-    welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-    //if this socket creation is not successful
-    if (!ISVALIDSOCKET(welcomeSocket)) 
-    {
-        printf("Failed to create Socket.\n");
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+        perror("Unable to create socket");
+        exit(EXIT_FAILURE);
     }
-    else
-    {
-        printf("Created Socket.\n");
-        
-        //The following code was copied exactly from https://www.programminglogic.com/example-of-client-server-program-in-c-using-sockets-and-tcp/
-        //However this seems to be setting up where and how the 
-        //the client will be conecting with the server
 
-        /*---- Configure settings of the server address struct ----*/
-        /* Address family = Internet */
-        serverAddr.sin_family = AF_INET;
-        /* Set port number, using htons function to use proper byte order */
-        serverAddr.sin_port = htons(9004);
-        /* Set IP address to localhost */
-        serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-        /* Set all bits of the padding field to 0 */
-        memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("Unable to bind");
+        exit(EXIT_FAILURE);
+    }
 
-        /*---- Bind the address struct to the socket ----*/
-        //binds welcomeSocket to serveraddress information found in the serverAddr var
-        bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    if (listen(s, 1) < 0) {
+        perror("Unable to listen");
+        exit(EXIT_FAILURE);
+    }
 
+    return s;
+}
 
+SSL_CTX *create_context()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
 
-        //this waits for up to 5 connection on the welcome socket
-        if (listen(welcomeSocket, 5) == 0) 
-        {
-            printf("Listening...\n");
-        }
-        else
-        {
-        printf("Error\n");        
-        }
-        
-        printf("Waiting for connections...\n");
+    method = TLS_server_method();
 
-        //this stores the max number of Sockets we can have
-        SOCKET max_socket = welcomeSocket;
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
 
-        //this will loop for forever
-        while(1) 
-        {
+    return ctx;
+}
 
-            //this loops through all of the posible connections on the socket
-            SOCKET i;
-            for(i = 1; i <= max_socket; ++i) {
+void configure_context(SSL_CTX *ctx)
+{
+    /* Set the key and cert */
+    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
 
-                //if we find  a connections
-                if (i == welcomeSocket) 
-                { 
-                    //declare some vars for latter use                         
-                    struct sockaddr_in new_addr;
-                    socklen_t addr_size = sizeof(new_addr);
-
-
-                    //accept an incoming connection on welcomeSocket that stores client address
-                    //in new_addr and stores the size in addr_size
-                    SOCKET socket_client = accept(welcomeSocket, (struct sockaddr*)&new_addr, &addr_size);
-
-                    printf("Connected\n");
-
-                    int expectedSeqNum = 0;
-                    
-                    //declare file we will be saving to
-                    FILE *file = fopen("Received.txt", "w");
-
-                    //while we have more data to recive
-                    while (1) 
-                    {   //take the data recived on socket_client and store in the buffer
-                        int anyMoreDataRecived = recv(socket_client, buffer, 1024, 0);
-                        printf("Data recived\n");
-                        if (anyMoreDataRecived <= 0)
-                        {
-                            printf("Finished Reciving All Data\n");
-                            break;
-                        }
-
-                        //seperate the sequence number from the data
-                        char* dataRecived = buffer + 10;
-                        char seqNum[10];
-                        strncpy(seqNum, buffer, 10);
-                        for(int i = 0; i<10; i++)
-                        {
-                            if(seqNum[i]=='-')
-                            {
-                                seqNum[i] = '\0';
-                            }
-                        }
-
-                        //if this was the data we expeceted 
-                        if(atoi(seqNum)==expectedSeqNum)
-                        {
-                            //prints data to file
-                            fputs(dataRecived, file);
-
-                            size_t dataSizeSend = send(socket_client, seqNum, 10, 0);
-
-                            expectedSeqNum++;
-                        }
-                        //if not the data we expect
-                        else
-                        {
-                            //tell it what data we need
-                            int temp = expectedSeqNum-1;
-                            char dataToSend[10];
-                            sprintf(dataToSend, "%d", temp);
-                            size_t dataSizeSend = send(socket_client, dataToSend, 10, 0);
-
-                        }
-
-                        //clear out datastructs
-                        bzero(seqNum, 10);
-                        bzero(buffer, 1024);
-                    }
-                    
-                    //closes the file
-                    fclose(file);
-
-                }
-
-            }
-
-        } //while(1)
-
-
-        printf("Closing listening socket...\n");
-        CLOSESOCKET(welcomeSocket);
-
-        printf("Finished.\n");
-        return 0;
+    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
     }
 }
 
+int main(int argc, char **argv)
+{
+    int sock;
+    SSL_CTX *ctx;
+
+    /* Ignore broken pipe signals */
+    signal(SIGPIPE, SIG_IGN);
+
+    ctx = create_context();
+
+    configure_context(ctx);
+
+    sock = create_socket(4433);
+
+    /* Handle connections */
+    while(1) {
+        struct sockaddr_in addr;
+        unsigned int len = sizeof(addr);
+        SSL *ssl;
+        const char reply[] = "test\n";
+
+        int client = accept(sock, (struct sockaddr*)&addr, &len);
+        if (client < 0) {
+            perror("Unable to accept");
+            exit(EXIT_FAILURE);
+        }
+
+        ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, client);
+
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+        } else {
+            SSL_write(ssl, reply, strlen(reply));
+        }
+
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+        close(client);
+    }
+
+    close(sock);
+    SSL_CTX_free(ctx);
+}
